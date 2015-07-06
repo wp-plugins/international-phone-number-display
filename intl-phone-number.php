bbe624f08e70cab4a4c8b4346fa3b2baf385ba15
paulfp <paulfp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-06 11:48:42 +0000

    Tagging version 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/international-phone-number-display/tags/2.0.0@1193298 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- intl-phone-number.php
+++ intl-phone-number.php
@@ -4,7 +4,7 @@
     Plugin URI: http://www.paulfp.net/wordpress-plugins/international-phone-number-display/
     Description: Automatically display telephone numbers in either standard national format or full international format (including dialling prefix) depending on where your web page is being viewed from.
     Author: Paul Freeman-Powell
-    Version: 1.0.3
+    Version: 2.0.0
     Author URI: https://twitter.com/paulfp
     */
 
@@ -24,6 +24,9 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+define("TC_API_BASE_URL", "https://api.telecomscloud.com");
+
 function intPnd_admin_actions() {
 	add_options_page("International Phone Number Display", "International Phone Number Display", "manage_options", "InternationalPhoneNumberDisplay", "intPnd_admin");
 }
@@ -36,8 +39,24 @@ function intPnd_tag_func( $atts ) {
 		'location' => 'GB'
     ), $atts );
 	
+	$args = array(
+    'timeout'     => 30,
+    'redirection' => 5,
+    'httpversion' => '1.0',
+    'user-agent'  => 'WordPress/' . $wp_version . '; ' . get_bloginfo( 'url' ),
+    'blocking'    => true,
+    'headers'     => array(),
+    'cookies'     => array(),
+    'body'        => null,
+    'compress'    => false,
+    'decompress'  => true,
+    'sslverify'   => true,
+    'stream'      => false,
+    'filename'    => null
+);
+	
 	$url = 'http://ipinfo.io/' . $_SERVER['REMOTE_ADDR'] . '/json';
-	$data = wp_remote_get($url);
+	$data = wp_remote_get($url, $args);
 	$countryCode = json_decode($data['body'])->country;
 	
 	if($data['response']['code'] === 200) {
@@ -50,18 +69,127 @@ add_shortcode( 'intPnd', 'intPnd_tag_func' );
 
 function formatNumberForInternational($serviceNumber, $location, $dialedFrom) {
 
-$url = 'https://api.telecomscloud.com/v1/format/number/international/'.$serviceNumber.'/'.$location.'/'.$dialedFrom.'?pretty=true';
+// check for cached value
+$cachedValue = get_option('intPnd_'.$serviceNumber.'_'.$location.'_'.$dialedFrom);
+if($cachedValue !== FALSE) {
+	return $cachedValue;
+}
 
-$headers = array( 'Authorization' => 'Basic ' . base64_encode(get_option('intPnd_sid').":".get_option('intPnd_token') ) );
-$result = wp_remote_get( $url, array( 'headers' => $headers ) );
+$endPoint = '/v1/format/number/international/'.$serviceNumber.'/'.$location.'/'.$dialedFrom.'?pretty=true';
+$url = TC_API_BASE_URL . $endPoint;
+
+$apiToken = intPnd_token();
+if(!$apiToken) {
+	return $serviceNumber;
+}
+
+$url .= '&access_token=' . $apiToken;
+
+ $args = array(
+    'timeout'     => 30,
+    'redirection' => 5,
+    'httpversion' => '1.0',
+    'user-agent'  => 'WordPress/' . $wp_version . '; ' . get_bloginfo( 'url' ),
+    'blocking'    => true,
+    'headers'     => array(),
+    'cookies'     => array(),
+    'body'        => null,
+    'compress'    => false,
+    'decompress'  => true,
+    'sslverify'   => true,
+    'stream'      => false,
+    'filename'    => null
+);
+
+$result = wp_remote_get( $url, $args );
+
+if(count($result->errors) > 0) {
+	return $serviceNumber;
+}
 $responseBody = json_decode($result['body']);
 
 	if($result['response']['code'] === 200) {
+		// cache for future identical requests
+		update_option('intPnd_'.$serviceNumber.'_'.$location.'_'.$dialedFrom, $responseBody->number);
+		
+		// return for showing now
 		return $responseBody->number;
 	} else {
 		return $serviceNumber; // fallback if unable to get response from API
 	}
 
+}
+
+function intPnd_token() {
+
+	// Get last saved Token & Expiry from WP Database
+	$TelecomsCloudAPI_accessToken = get_option('TelecomsCloudAPI_accessToken');
+	$TelecomsCloudAPI_accessToken_expiry = get_option('TelecomsCloudAPI_accessToken_expiry');
+	
+	$currentDate = new DateTime('now');
+	$now = $currentDate->format('Y-m-d H:i:s');
+	
+	if( (!$TelecomsCloudAPI_accessToken) OR (!$TelecomsCloudAPI_accessToken_expiry) OR ($TelecomsCloudAPI_accessToken_expiry < $now) ) {
+		// Token expired, get a new one and save it etc.
+		$client_id = get_option('intPnd_clientID');
+		$client_secret = get_option('intPnd_clientSecret');
+		
+		if( (!$client_id) OR (!$client_secret) ) {
+			return false;
+		}
+		
+		return intPnd_getNewToken($client_id, $client_secret);
+	} else {
+		// Current token still valid for use
+		return $TelecomsCloudAPI_accessToken;
+	}
+
+}
+
+function intPnd_getNewToken($client_id, $client_secret) {
+	// Validate API Credentials provided
+	$endPoint = "/v1/authorization/oauth2/grant-client";
+	$url = TC_API_BASE_URL . $endPoint;
+
+	//open connection
+	$ch = curl_init();
+
+	$fields = array(
+		'client_id' => urlencode($client_id),
+		'client_secret' => urlencode($client_secret)
+		);
+	$fields_string = json_encode($fields, true);
+
+	//set the url, number of POST vars, POST data
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch,CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json',
+		'Content-Length: ' . strlen($fields_string))
+	);
+	curl_setopt($ch,CURLOPT_POST, count($fields));
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+
+	//execute post
+	$result = curl_exec($ch);
+
+	//close connection
+	curl_close($ch);
+
+	$result = json_decode($result, true);
+	
+	if(array_key_exists('error', $result)) {
+		return false;
+	} else {
+		
+		$expiresDate = new DateTime('now');
+		$expiresDate->add(new DateInterval('PT' . $result['expires_in'] . 'S'));
+		$expiresDate = $expiresDate->format('Y-m-d H:i:s');
+		update_option('TelecomsCloudAPI_accessToken', $result['access_token']);
+		update_option('TelecomsCloudAPI_accessToken_expiry', $expiresDate);
+			
+		return $result['access_token'];
+	}
 }
 
 function intPnd_admin() {
@@ -78,19 +206,28 @@ function intPnd_admin() {
 	<?php
 	if($_POST['intPnd_hidden'] == 'Y') {
         //Form data sent
-        $intPnd_sid = trim($_POST['intPnd_sid']);
-		update_option('intPnd_sid', $intPnd_sid);
-         
-        $intPnd_token = trim($_POST['intPnd_token']);
-		update_option('intPnd_token', $intPnd_token);  
-        ?>
-        <div class="updated"><p><strong>Options saved.</strong></p></div>
-        <?php
+        
+		$intPnd_clientID = trim($_POST['intPnd_clientID']);
+		$intPnd_clientSecret = trim($_POST['intPnd_clientSecret']);
+		
+		$result = intPnd_getNewToken($intPnd_clientID, $intPnd_clientSecret);
+		
+		if(!$result) {
+			?>
+			<div class="error"><p><strong>The Credentials you entered for the Telecoms Cloud API were not valid. Please check them and try again.</strong></p></div>
+			<?php
+		} else {
+			update_option('intPnd_clientID', $intPnd_clientID);
+			update_option('intPnd_clientSecret', $intPnd_clientSecret);
+			?>
+			<div class="updated"><p><strong>Options saved.</strong></p></div>
+			<?php
+		}
     } else {
-		$intPnd_sid = get_option('intPnd_sid');
-		$intPnd_token = get_option('intPnd_token');
-}       
-	   ?>
+		$intPnd_clientID = get_option('intPnd_clientID');
+		$intPnd_clientSecret = get_option('intPnd_clientSecret');
+	}
+?>
 	
 	<p>The plugin will automatically display telephone numbers in the correct format for international users from any country in the world. It does this by looking up their IP address (using <a href="http://ipinfo.io" target="_blank">ipinfo.io</a>) and determining the country, then passing this information along with the phone number to the <a href="https://www.telecomscloud.com/api" target="_blank">Telecoms Cloud API</a> which formats the number correctly and adds the appropriate International Direct Dialing Number (IDD).</p>
 	
@@ -111,8 +248,16 @@ function intPnd_admin() {
         <input type="hidden" name="intPnd_hidden" value="Y">
         <h4>Your Telecoms Cloud API Settings</h4>
 		<p>In order for the conversion to take place, you must enter your API access keys below.</p>
-        <p><label>SID: <input type="text" name="intPnd_sid" value="<?php echo $intPnd_sid; ?>" size="30"></label></p>
-        <p><label>Token: <input type="text" name="intPnd_token" value="<?php echo $intPnd_token; ?>" size="30"></label></p>
+        <p><label>Client ID: <input type="text" name="intPnd_clientID" value="<?php echo $intPnd_clientID; ?>" size="30" required="required" /></label></p>
+        <p><label>Client Secret: <input type="text" name="intPnd_clientSecret" value="<?php echo $intPnd_clientSecret; ?>" size="30" required="required" /></label></p>
+		
+		<?php
+			$TelecomsCloudAPI_accessToken = get_option('TelecomsCloudAPI_accessToken');
+			$TelecomsCloudAPI_accessToken_expiry = get_option('TelecomsCloudAPI_accessToken_expiry');
+			if( ($TelecomsCloudAPI_accessToken !== FALSE) AND ($TelecomsCloudAPI_accessToken_expiry !== FALSE) ) {
+		?>
+		<p>Your current access token is <strong><?=$TelecomsCloudAPI_accessToken;?></strong> which expires on <strong><?=$TelecomsCloudAPI_accessToken_expiry;?></strong>. The plugin will automatically request a new access token when this expires.</p>
+		<?php } ?>
 		
 		<p>Get your API access keys for free at <a href="https://www.telecomscloud.com/sign-up.html?api">https://www.telecomscloud.com/sign-up.html?api</a></p>	
      
